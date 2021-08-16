68b793a4aa03a4c54b85133fcb6b911ccdfa9996
Cooper Saye <coopersaye@gmail.com>
2021-08-15 22:45:09 -0700

    Import project data.

---
+++ data/projects.js
@@ -0,0 +1,14 @@
+export const projects = [
+  {
+    title: "Codetrain",
+    description: "An educational browser game engine.",
+    link: "codetrain.org",
+    github: "csaye/codetrain"
+  },
+  {
+    title: "Devcomms",
+    description: "Team communication and coordination all in one place.",
+    link: "devcomms.io",
+    github: "csaye/devcomms"
+  }
+];
