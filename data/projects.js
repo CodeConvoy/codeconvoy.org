2c34eea59563f58e8d7889a736f798cb2905c656
Cooper Saye <coopersaye@gmail.com>
2021-09-08 20:29:24 -0700

    Update project links.

--- data/projects.js
+++ data/projects.js
@@ -3,12 +3,12 @@ export const projects = [
     title: "Codetrain",
     description: "An educational browser game engine.",
     link: "codetrain.org",
-    github: "csaye/codetrain"
+    github: "codeconvoy/codetrain"
   },
   {
     title: "Devcomms",
     description: "Team communication and coordination all in one place.",
     link: "devcomms.io",
-    github: "csaye/devcomms"
+    github: "codeconvoy/devcomms"
   }
 ];
