e60a1f6e765434d5ca6a8a3fc9021a5d59b02545
Cooper Saye <coopersaye@gmail.com>
2021-12-02 11:51:33 -0800

    Improve 404 page styling.

--- pages/404.js
+++ pages/404.js
@@ -4,7 +4,7 @@ import styles from '../styles/pages/404.module.css';
 
 export default function Page404() {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>404 <span>| Page not found</span></h1>
       <Link href="/">
         <a>Return home</a>
